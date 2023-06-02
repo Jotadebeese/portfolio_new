fa8d25bdea1333dd6dcdca44fa18cf457d565084
Jotadebeese <jotadebeese@gmail.com>
2023-06-02 22:04:28 +1000

    Updated navbar and created styles folder for styling files

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -1,18 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
-import styles from '/app/NavMenu.module.css'
+import styles from '/styles/NavMenu.module.css'
 
 export default function NavMenu() {
     return (
         <nav className={styles.nav}>
+            <div className={styles.pagina}>
             <Link href={'/'}>
-                <Image
-                    src='/corgi.png'
-                    width={30}
-                    height={30}
-                    alt="Juan's face"
-                />
-            </Link>
+                    <Image
+                        src='/corgi.png'
+                        width={50}
+                        height={50}
+                        alt="Juan's face"
+                    />
+
+                </Link>
+                <p><i>By</i></p>
+
+                    <Link href="/">
+                        <button><i>PAGINA</i></button>
+                    </Link>
+                </div>
             <ul className={styles.links}>
                 <li>
                     <Link href={'/about'}>Me</Link>
