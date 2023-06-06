7889edbbdea462ccd7ff57c1e275383bb2cde2bb
Jotadebeese <jotadebeese@gmail.com>
2023-06-06 21:52:43 +1000

    Playing around

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,7 +1,7 @@
 export default function AboutMe() {
     return (
         <div className="aboutme">
-            <h2>Hi, I'm Juan</h2>
+            <h1>Hi, I'm Juan</h1>
             <p>A Software Developer, with background in electronics engineering and a big passion in AI.</p>
             <small>An all-rounder... huh?</small>
         </div>
--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -1,14 +1,16 @@
 export default function ProjectsList() {
     return (
-        <div className="projects-list">
-            <h3>My projects</h3>
-            <p>Feel free to look at them, in deep...</p>
-            <ul id="projects-list">
-                <li>
-                    <h3>PAGINA - Social Media App</h3>
-                    <p>A social Media app where users can create accounts, create posts using markdown, edit, publish and unpublish, like and dislike and see others users posts. </p>
-                </li>
-            </ul>
+        <div className="projects-container">
+            <div className="projects-list">
+                <h1>My projects</h1>
+                <p>Feel free to look at them, in deep...</p>
+                <ul id="projects-list">
+                    <li>
+                        <h3>PAGINA - Social Media App</h3>
+                        <p>A social Media app where users can create accounts, create posts using markdown, edit, publish and unpublish, like and dislike and see others users posts. </p>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
