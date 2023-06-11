e191262d203e6e4850fd679e9b9edab7caa3cff0
Jotadebeese <jotadebeese@gmail.com>
2023-06-11 21:18:26 +1000

    Revert "Removed AuthJS"
    This reverts commit f04bdafdc7e679e1f8b5799b733cc35f485055ba.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import '/styles/globals.css'
 import { PT_Serif } from 'next/font/google'
 import NavMenu from '@/components/NavMenu'
-
+import AuthProvider from './AuthProvider'
 
 const pt_serif = PT_Serif({ subsets: ['latin'], weight:['400'] })
 
@@ -16,13 +16,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-
+    <AuthProvider>
       <html lang="en">
         <body className={pt_serif.className}>
           <NavMenu />
           {children}
         </body>
       </html>
-
+    </AuthProvider>
   )
 }
