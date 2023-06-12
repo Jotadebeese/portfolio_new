5fa7e972711a99215a1dc174237df33c3201caa5
Jotadebeese <jotadebeese@gmail.com>
2023-06-12 14:53:02 +1000

    Fixing little error

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from '/styles/NavMenu.module.css'
-import { SignInButton, SignOutButton } from "./buttons";
 
 export default function NavMenu() {
     return (
