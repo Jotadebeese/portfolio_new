68317496f557b29262cdfd45980d89aecef9a7a4
Jotadebeese <jotadebeese@gmail.com>
2023-06-12 14:38:20 +1000

    Fixing 3

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import type { NextAuthOptions } from 'next-auth';
+import { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions: NextAuthOptions = {
