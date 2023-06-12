ba01dc90efd6e371dd43f1215bfcc9f121adcf3b
Jotadebeese <jotadebeese@gmail.com>
2023-06-12 14:37:00 +1000

    Fixing 2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
-import NextAuth from 'next-auth/next';
+import NextAuth from 'next-auth';
+import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
-export const auth = {
+export const authOptions: NextAuthOptions = {
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
@@ -10,5 +11,5 @@ export const auth = {
     ],
 }
 
-const handler = NextAuth(auth);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
