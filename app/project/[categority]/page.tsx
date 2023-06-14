81c4b16b004f6cc3598b5bd70b903fd5c495ab99
Jotadebeese <jotadebeese@gmail.com>
2023-06-14 22:06:21 +1000

    Fixed sme styling

--- app/project/[categority]/page.tsx
+++ app/project/[categority]/page.tsx
@@ -7,18 +7,16 @@ export default function ProjectsList(categority: any) {
     const category = categority.params.categority;
 
     return (
-        <div className="projects-container">
-                <div className="projects-list">
-                    <h2>Project List</h2>
-                    <p>Feel free to look at them.</p>
-                    <div className="little-box">
-                        <Link href={'/project/full_stack'} className={category === 'full_stack'? 'btn select' : 'btn'} >Full Stack</Link>
-                        <Link href={'/project/machine'} className={category === 'machine'? 'btn select' : 'btn'} >Machine Learning</Link>
-                        <Link href={'/project/embedded'} className={category === 'embedded'? 'btn select' : 'btn'} >Embedded Systems</Link>
-                    </div>
-                    {/* @ts-expect-error Server Component */}
-                    <ProjectsCard categority={categority.params.categority} />
-                </div>
-      </div>
+        <div className="projects-container fade-in">
+            <h2>Projects</h2>
+            <p>Divided in three categories:</p>
+            <div className="little-box">
+                <Link href={'/project/machine'} className={category === 'machine'? 'btn select' : 'btn'} >Machine Learning</Link>
+                <Link href={'/project/full_stack'} className={category === 'full_stack'? 'btn select' : 'btn'} >Full Stack</Link>
+                <Link href={'/project/embedded'} className={category === 'embedded'? 'btn select' : 'btn'} >Embedded Systems</Link>
+            </div>
+            {/* @ts-expect-error Server Component */}
+            <ProjectsCard categority={categority.params.categority} />
+        </div>
     );
 }
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -10,17 +10,15 @@ export default function ProjectsList() {
 
     return (
         <div className="projects-container fade-in">
-            <div className="projects-list">
-                    <h2>Project List</h2>
-                    <p>Feel free to look at them.</p>
-                    <div className="little-box">
-                        <Link href={'/project/full_stack'} className="btn select" >Full Stack</Link>
-                        <Link href={'/project/machine'} className="btn" >Machine Learning</Link>
-                        <Link href={'/project/ebedded'} className="btn" >Embedded Systems</Link>
-                    </div>
-                    {/* @ts-expect-error Server Component */}
-                    <ProjectsCard categority={'full_stack'} />
+            <h2>Projects</h2>
+            <p>Divided in three categories:</p>
+            <div className="little-box">
+                <Link href={'/project/machine'} className="btn" >Machine Learning</Link>
+                <Link href={'/project/full_stack'} className="btn select" >Full Stack</Link>
+                <Link href={'/project/ebedded'} className="btn" >Embedded Systems</Link>
             </div>
+            {/* @ts-expect-error Server Component */}
+            <ProjectsCard categority={'full_stack'} />
       </div>
     );
 }
