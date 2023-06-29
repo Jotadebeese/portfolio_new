384c0c149d444ff6f593cfc32e2192d510b026e8
Jotadebeese <jotadebeese@gmail.com>
2023-06-29 20:37:50 +1000

    c

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className='not-found fade-in'>
         <span className='box-404'>404</span>
-        <p>This page does not exist<br />or there is some<br />Work in progress.</p>
+        <p>This page might not exist,<br />or some work<br />might be in progress.</p>
         <br/>
         <Link  className='btn' href={'/'}>
             Go back...
