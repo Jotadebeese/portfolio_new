6ed9c2fda430de7dd34cc9c82e42f027f900624e
Jotadebeese <jotadebeese@gmail.com>
2023-07-07 21:23:55 +1000

    Updated error.tsx refresh

--- app/error.tsx
+++ app/error.tsx
@@ -2,18 +2,19 @@
  
 import { useEffect } from 'react';
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
  
 export default function Error({
   error,
-  reset,
 }: {
   error: Error
-  reset: () => void
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error)
   }, [error])
+
+  const router = useRouter()
  
   return (
     <div className='error-container fade-in'>
@@ -22,7 +23,7 @@ export default function Error({
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
-          () => reset()
+          () => router.refresh()
         }
       >
         Try again
