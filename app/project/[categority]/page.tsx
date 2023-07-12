94cc5a114deca4c36d8e6a80e24699da08a7e191
Jotadebeese <jotadebeese@gmail.com>
2023-07-12 16:30:42 +1000

    Revert "Fixed prisma issue"
    This reverts commit a006b56c50069d2f396f5cd3ed6d378a1876d2b1.

--- app/project/[categority]/page.tsx
+++ app/project/[categority]/page.tsx
@@ -1,16 +1,12 @@
+import LoadingUsers from "./loading";
 import ProjectsCard from "@/components/ProjectsCard";
-import Link from "next/link";
+import { Suspense } from "react";
+
 
 export default function ProjectsList(categority: any) {
-    const category = categority.params.categority;
+
     return (
-        <>  
-            <p>Choose one:</p>
-            <div className="little-box">
-                <Link href={'/project/machine'} className={category === 'machine'? 'btn select' : 'btn'} >Machine Learning</Link>
-                <Link href={'/project/full_stack'} className={category === 'full_stack'? 'btn select' : 'btn'} >Full Stack</Link>
-                <Link href={'/project/embedded'} className={category === 'embedded'? 'btn select' : 'btn'} >Embedded Systems</Link>
-            </div>
+        <>
             {/* @ts-expect-error Server Component */}
             <ProjectsCard categority={categority} />            
         </>
