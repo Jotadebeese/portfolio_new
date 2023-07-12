a006b56c50069d2f396f5cd3ed6d378a1876d2b1
Jotadebeese <jotadebeese@gmail.com>
2023-07-12 16:19:53 +1000

    Fixed prisma issue

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function MyInformation({
     children, // will be a page or nested layout
   }: {
--- app/me/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default async function LoadingUsers() {
-    return (
-    <div className="loader-container fade-in">
-        <img src="/temp.gif" />
-        <div className="loader"></div>
-        <p>Loading  .  .  .</p>
-    </div>
-    );
-  }
