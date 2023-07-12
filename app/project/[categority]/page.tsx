8bb5b035c84cd2d0b46aff6fa4bf9df2aa1baed1
Jotadebeese <jotadebeese@gmail.com>
2023-07-12 14:56:36 +1000

    Updated NextJS and tried to fix issues with Prisma connection

--- app/project/[categority]/page.tsx
+++ app/project/[categority]/page.tsx
@@ -5,12 +5,10 @@ import { Suspense } from "react";
 
 export default function ProjectsList(categority: any) {
 
-    const category = categority.params.categority;
-
     return (
         <>
             {/* @ts-expect-error Server Component */}
-            <ProjectsCard categority={categority.params.categority} />            
+            <ProjectsCard categority={categority} />            
         </>
     );
 }
